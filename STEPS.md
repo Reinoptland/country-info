15698581579b461e47ee8457590a1b2ef9f0e3ec
Reinoptland <reinoptland@gmail.com>
2021-02-12 15:17:31 +0100

    Make function, call in async function

--- STEPS.md
+++ STEPS.md
@@ -34,9 +34,9 @@ _Tip:_ Als de de [documentatie](https://www.npmjs.com/package/axios) bekijkt en 
 
 3. Maak op basis van de response de volgende string en log dit in de console: `The capital is [city]`
 
-- [ ] Goed in de data kijken, waar zit deze info: -> loggen
-- [ ] variabelen maken: countryCapital
-- [ ] template string maken met -> `${countryCapital} ... etc`
+- [x] Goed in de data kijken, waar zit deze info: -> loggen
+- [x] variabelen maken: countryCapital
+- [x] template string maken met -> `${countryCapital} ... etc`
 
 4. Maak een functie die ongeacht het aantal currencies die in een land gebruikt worden, een string maakt. In een land kunnen één of twee currencies gebruikt worden:
 - 1 valuta: `and you can pay with [currency]'s`
--- main.js
+++ main.js
@@ -3,19 +3,24 @@ async function getCountryInfo() {
   const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
 
   const response = await axios.get(url);
-  console.log(response); // -> Countries!
+  // console.log(response); // -> Countries!
   // JE KRIJGT EEN ARRAY - niet een object
-  console.log(response.data[0].name);
+  // console.log(response.data[0].name);
   const countryData = response.data[0]
 
-  console.log(countryData)
+  // console.log(countryData)
   // 2
   const geography = `${countryData.name} is situated in ${countryData.subregion}. It has a population of ${(countryData.population / 1000000).toFixed(1)} million people`
   const capital = `The capital is ${countryData.capital}`
 
-  console.log("OUTPUT:", capital)
+  // console.log("OUTPUT:", capital)
+  formatCurrencies()
 }
 
 const searchButton = document.getElementById("searchButton");
 searchButton.addEventListener("click", getCountryInfo);
+
+function formatCurrencies(){
+  console.log('hi')
+}
 
